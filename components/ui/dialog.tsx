3ddbcbf1ec56dea92c84d6667266f74d7ae4f904
a.ito <10333049+BoxPistols@users.noreply.github.com>
2025-11-15 07:49:13 +0900

    Update dialog.tsx

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -36,7 +36,7 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-lg opacity-70 ring-offset-white dark:ring-offset-gray-800 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-gray-950 dark:focus:ring-gray-300 focus:ring-offset-2 disabled:pointer-events-none hover:bg-gray-100 dark:hover:bg-gray-700 text-gray-900 dark:text-gray-100">
+      <DialogPrimitive.Close className="z-10 absolute right-4 top-4 rounded-lg opacity-70 ring-offset-white dark:ring-offset-gray-800 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-gray-950 dark:focus:ring-gray-300 focus:ring-offset-2 disabled:pointer-events-none hover:bg-gray-100 dark:hover:bg-gray-700 text-gray-900 dark:text-gray-100">
         <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
